a6bc0f492f2adc617bc0568ab6e4feb6ff12bc9f
alanionita <a.g.ionita@gmail.com>
2018-01-25 23:58:15 +0000

    added floatingButton + action, an a new map

--- app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.android.helloandroid;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 
 public class MainActivity extends Activity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void messageUs(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, "contact@udacity.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Hello Udacity! 👋");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
