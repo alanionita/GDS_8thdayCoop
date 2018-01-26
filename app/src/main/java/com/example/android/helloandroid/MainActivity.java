b2892184380a691ade18d0793f8d98888ea22e7d
alanionita <a.g.ionita@gmail.com>
2018-01-26 21:00:49 +0000

    adds tweak to MainActivity so that it uses AppCompatActivity instead of Activity

--- app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -9,7 +9,7 @@ import android.util.Log;
 import android.view.View;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
