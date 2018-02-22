87fad19e5d5c976d1de68b69f5fe092d63907315
alanionita <a.g.ionita@gmail.com>
2018-02-22 19:35:35 +0000

    adds sticky Youtube view on main layout, a split layout: scrolling and otherwise and most of the text content

---
+++ app/src/main/java/com/example/android/helloandroid/Config.java
@@ -0,0 +1,15 @@
+package com.example.android.helloandroid;
+
+/**
+ * Created by alanionita on 19/02/2018.
+ */
+
+public final class Config {
+
+    private Config() {
+
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyAntxk6EPaZP94MACtA55Ld0nAbhG_kOdU";
+
+}
--- app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -7,23 +7,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
+    }
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            player.loadVideo("vEBub9AiTMM"); // Plays https://www.youtube.com/watch?v=vEBub9AiTMM
+        }
     }
 
-    public void messageUs(View view) {
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
-        intent.putExtra(Intent.EXTRA_EMAIL, "contact@udacity.com");
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Hello Udacity! 👋");
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), errorReason.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(Config.YOUTUBE_API_KEY, this);
+        }
+    }
+
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return youTubeView;
     }
 }
